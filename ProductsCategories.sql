8e7f16d587bf7bf0161c2927e2bfbc50adbab7c8
germanshpiker@gmail.com <Xpqrb6886>
2023-02-12 20:35:59 +0300

    *Add readme *Add SQL request

---
+++ ProductsCategories.sql
@@ -0,0 +1,4 @@
+﻿SELECT Products.ProductName, Categories.CategoryName FROM Products
+LEFT JOIN ProductsCategories ON Products.ProductId= ProductsCategories.ProductId
+LEFT JOIN Categories ON Category.CategoryId=ProductsCategories.PositionId
+ORDER BY ProductName
